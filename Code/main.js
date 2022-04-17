210063419120a7ec092066faa74f32690009faef
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-18 02:29:23 +0700

    update

---
+++ Code/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            brand:'/img/brand.png',
+            showcase1:'/img/showcase1.jpg',
+            showcase2:'/img/showcase2.jpg',
+            cart:'/img/cart.png',
+        }
+    }
+})
