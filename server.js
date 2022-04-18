4cff154ed316b045dc8d8b4eaead25792c373928
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-19 05:34:09 +0700

    update

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','ejs')
+
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open',() => console.log('Connected to Database'))
+
+
+app.get('/', (req, res) => {
+    res.render('../display/index');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('../display/login')
+});
+
+app.get('/register',(req,res)=>{
+  res.render('../display/register')
+});
+
+
+
+
+app.listen(3000, () => console.log('Server Started'))
