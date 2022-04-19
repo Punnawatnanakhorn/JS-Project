3e6ba71931cd9617e0807a6bcb6d40085a9a5071
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-19 11:26:58 +0700

    u

--- server.js
+++ server.js
@@ -31,6 +31,19 @@ app.use(express.json());
 
 app.use('/api/user',authRoute);
 
+app.post('/register',async(req,res)=>{
+  const user=new User({
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password
+  });
+  try{
+      const savedUser=await user.save();
+      res.send(savedUser);
+  }catch(err){
+      res.status(400).send(err);
+  }
 
+});
 
 app.listen(3000, () => console.log('Server Started'))
