84488511441706a387f00f369cf9f8d9ffba1941
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-20 01:59:37 +0700

    u

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
 app.use(express.static(__dirname+'/public'));
-app.set('view engine','ejs')
+app.set('view engine','ejs');
 
 
 
@@ -27,6 +29,12 @@ app.get('/register',(req,res)=>{
 
 
 app.post('/register',(req,res)=>{
+  const user = new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password
+  });
+  user.save();
   res.redirect('/');
 });
 
