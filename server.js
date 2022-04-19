23a92131362f77d87eec5734775f8c209bdc4633
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-20 01:27:45 +0700

    u

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 app.use(express.static(__dirname+'/public'));
 app.set('view engine','ejs')
 
-const authRoute=require('./routes/auth');
 
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -27,20 +26,10 @@ app.get('/register',(req,res)=>{
 });
 
 
-app.use(express.json());
-
-app.use('/api/user',authRoute);
 
 
 
-app.post('/register',async(req,res)=>{
-  const user=new User({
-    email: req.body.email,
-    name: req.body.name,
-    password: req.body.password
-  });
-  user.save();
-  res.redirect('/register');
-});
+
+
 
 app.listen(3000, () => console.log('Server Started'))
