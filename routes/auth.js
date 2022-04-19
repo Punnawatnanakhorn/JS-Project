556cd74393a4fcd1dac36ece0a41e59bd21fa209
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-19 11:38:00 +0700

    u

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 const router=require('express').Router();
 const User=require('../models/User');
 
-app.post('/register',async(req,res)=>{
-    res.send('This is fine');
-});
+
 
 module.exports=router;
