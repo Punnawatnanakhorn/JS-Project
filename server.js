0cd0b823fb1eb5d1276aae1477e9700325696d56
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-19 10:23:18 +0700

    update

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const mongoose = require('mongoose')
 app.use(express.static(__dirname+'/public'));
 app.set('view engine','ejs')
 
+const authRoute=require('./routes/auth');
+
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open',() => console.log('Connected to Database'))
+
+
+
+
+app.use(express.json());
+
+app.use('/api/user',authRoute);
+
+
 
 
 app.get('/', (req, res) => {
