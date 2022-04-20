317c9912596cede93e9a3b58cb848f7b44a1a0c2
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-20 07:52:24 +0700

    u

--- server.js
+++ server.js
@@ -38,11 +38,6 @@ app.post('/login',(req,res)=>{
 
 
 app.post('/register', async(req,res)=>{
-  const user = new User({
-    email: req.body.email,
-    name: req.body.name,
-    password: req.body.password
-  });
   res.redirect('/');
 });
 
